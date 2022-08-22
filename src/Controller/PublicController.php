60c60290ab168f68a1c665f8e883513b954f3d21
Samuel <slanguet@protonmail.com>
2022-08-22 10:36:30 +0200

    add form
    ajout du formulaire de contact

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class PublicController extends AbstractController
 {
@@ -27,8 +30,15 @@ class PublicController extends AbstractController
     #[Route('/contact', name: 'contact')]
     public function contact(): Response
     {
-        return $this->render('public/whoami.html.twig', [
+        $formulaire = $this->creatFormBuilder()
+        ->add('Nom', TextType::class,
+            ['attr' => ['class' => 'formCss']])
+        ->add('Envoyer', SubmitType::class)
+        ->getForm();
+
+        return $this->render('public/contact.html.twig', [
             'controller_name' => 'Who am I?',
+            'frmContact' => $formulaire->createView()
         ]);
     }
 }
