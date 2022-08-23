d001c17d7906ed7d267a785d65b5698235fd41f7
Samuel <slanguet@protonmail.com>
2022-08-23 13:36:57 +0200

    update

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -2,19 +2,13 @@
 
 namespace App\Controller;
 
-use App\Form\ContactFormType;
+use App\Entity\Messages;
+use App\Form\MessagesType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\TelType;
-use Symfony\Component\Form\Extension\Core\Type\ResetType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class PublicController extends AbstractController
 {
@@ -37,40 +31,24 @@ class PublicController extends AbstractController
     #[Route('/contact', name: 'contact')]
     public function contact(Request $request, EntityManagerInterface $entityManager): Response
     {
-        /*$formulaire = $this->createFormBuilder()
-            ->add('Nom', TextType::class,
-                ['attr' => ['class' => 'formCss']])
-            ->add('Prenom', TextType::class)
-            ->add('Mail', EmailType::class)
-            ->add('Entreprise', TextType::class,
-                ['required' => false])
-            ->add('Telephone', TelType::class)
-            ->add('Objet_du_message', ChoiceType::class,
-                ['choices' => ['machine' => 'message', 'truc' => 'truc', 'bidule' => 'bidule']])
-            ->add('Message', TextareaType::class)
-            ->add('Envoyer', SubmitType::class)
-            ->add('Annuler', ResetType::class)
-            ->setMethod('post')
-            ->setAction('/')
-            ->getForm();
+        $messages = new Messages();
+        
+        $contactFormulaire = $this->createForm(MessagesType::class, $messages);
+        // dd($contactFormulaire);
+        $contactFormulaire->handleRequest($request);
 
-        $formulaire->handleRequest($request);
+        // dd($contactFormulaire);
 
-        if ($formulaire->isSubmitted() && $formulaire->isValid()) {
-            $entityManager->persist($formulaire);
-            $entityManager->flush();
-            //dd($formulaire->getData()); // Récupère les datas du formulaire sous forme de tableau associatif
+        if ($contactFormulaire->isSubmitted() && $contactFormulaire->isValid()) {
+            $entityManager->persist($contactFormulaire);
+            $entityManager->flush($contactFormulaire);
+
+            return new Response('Ayé enfin');
         }
 
         return $this->render('public/contact.html.twig', [
-            'controller_name' => 'Who am I?',
-            'frmContact' => $formulaire->createView()
-
-        ]);*/
-        $contactFormulaire = $this->createForm(ContactFormType::class);
-        return $this->render('public/contact.html.twig',[
             'controller_name' => 'Formulaire de contact qui marche',
-            'frmContact' => $contactFormulaire->createView()]);
-
+            'frmContact' => $contactFormulaire->createView()
+        ]);
     }
 }
